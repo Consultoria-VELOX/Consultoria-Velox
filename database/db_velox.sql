689e52c1d2865e6c00f8679dac9bc9956fea56bc
Victor Moreno <victoormorenoo20@gmail.com>
2026-06-04 20:56:33 -0300

    Criação de trigger no banco de dados
    Trigger trg_usuario_cargo é ativada em todo novo usuário cadastrado, se o id do usuario cadastrado for 1 ele vira admin, senão vira cliente automaticamente.

--- database/db_velox.sql
+++ database/db_velox.sql
@@ -1,5 +1,4 @@
 CREATE DATABASE db_velox;
-
 USE db_velox;
 
 CREATE TABLE tb_usuarios (
@@ -17,18 +16,36 @@ nome_cargo VARCHAR(15) NOT NULL
 );
 
 CREATE TABLE tb_usuario_cargo (
-	id_usuario INT NOT NULL,
+	id_usuario INT UNIQUE NOT NULL,
     id_cargo INT NOT NULL,
     FOREIGN KEY (id_usuario) REFERENCES tb_usuarios(id_usuario),
     FOREIGN KEY (id_cargo) REFERENCES tb_cargo(id_cargo)    
 );
 
+DELIMITER //
+CREATE TRIGGER trg_usuario_cargo
+AFTER INSERT 
+ON tb_usuarios
+FOR EACH ROW 
+BEGIN 
+	IF NEW.id_usuario = 1 THEN
+		INSERT INTO tb_usuario_cargo (id_usuario, id_cargo)
+        VALUES (NEW.id_usuario, 1);
+	ELSE 
+		INSERT INTO tb_usuario_cargo (id_usuario, id_cargo)
+        VALUES (NEW.id_usuario, 3);
+	END IF;
+END//
+
+DELIMITER ;
+        
 CREATE TABLE tb_estoque_veiculos (
 	id_veiculo INT PRIMARY KEY AUTO_INCREMENT,
     marca_veiculo VARCHAR(20) NOT NULL,
     modelo_veiculo VARCHAR(50) NOT NULL,
     ano INT NOT NULL,
     placa_veiculo CHAR(7) NOT NULL UNIQUE,
+    preco DOUBLE(8,2) NOT NULL,
     descricao TEXT NOT NULL,
     disponivel BOOLEAN NOT NULL
 );
@@ -50,18 +67,20 @@ CREATE TABLE tb_tickets(
 );
 
 INSERT INTO tb_cargo VALUES 
-("1", "Cliente"),
+("1", "Administrador"),
 ("2", "Gestor"),
-("3", "Administrador");
+("3", "Cliente");
 
 INSERT INTO tb_usuarios (nome_usuario, sobrenome_usuario, email, telefone, senha)
-VALUES ('Admin', 'Velox', 'admin@velox.com', '(00) 00000-0000', '123');
+VALUES ('Admin', 'Velox', 'adminn@velox.com', '(00) 00000-0000', '123');
 
-INSERT INTO tb_usuario_cargo VALUES (1, 3);
+INSERT INTO tb_usuarios (nome_usuario, sobrenome_usuario, email, telefone, senha)
+VALUES ('Gestor', 'Velox', 'gestor@velox.com', '(00) 00000-0000', '123');
 
-INSERT INTO tb_estoque_veiculos (marca_veiculo, modelo_veiculo, ano, placa_veiculo, descricao, disponivel)
-VALUES ('Chevrolet', 'Corsa', '2008', 'AAAAAAA', 'Em perfeito estado', true);
+INSERT INTO tb_estoque_veiculos (marca_veiculo, modelo_veiculo, ano, preco, placa_veiculo, descricao, disponivel)
+VALUES ('Chevrolet', 'Corsa', '2008', 10000.00 , 'AAAAABC', 'Em perfeito estado', true);
 
 SELECT*FROM tb_usuarios;
 SELECT*FROM tb_usuario_cargo;
 SELECT*FROM tb_tickets;
+SELECT*FROM tb_estoque_veiculos;
